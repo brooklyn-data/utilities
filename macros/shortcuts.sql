9c9084fe1b89e432b702612acafc8d95faffa8de
Scott Breitenother <sbreitenother@users.noreply.github.com>
2019-02-19 15:22:04 -0500

    Switch default timezone to `America/New_York`
    Account for day light saving time

--- macros/shortcuts.sql
+++ macros/shortcuts.sql
@@ -1,3 +1,3 @@
-{%- macro convert_tz (column_name, timezone='EST') -%}
-	convert_timezone('{{timezone}}', {{column_name}}) as {{column_name[(column_name.find('.')+1):] + '_' + timezone.lower()}}
+{%- macro convert_tz (column_name, timezone='America/New_York', timezone_alias='EST') -%}
+	convert_timezone('{{timezone}}', {{column_name}}) as {{column_name[(column_name.find('.')+1):] + '_' + timezone_alias.lower()}}
 {%- endmacro -%}
