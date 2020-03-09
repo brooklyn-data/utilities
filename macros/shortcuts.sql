8aa330e5ce12e245d7248ce8f70cd0ecefa2fa6a
Sean Rose <sean@brooklyndata.co>
2020-03-09 09:40:38 -0700

    Change `convert_tz` default arg values to be compatible with dbt 0.15.
    dbt 0.15 apparently only processes var replacements during the compile step and not during the run step.

--- macros/shortcuts.sql
+++ macros/shortcuts.sql
@@ -1,3 +1,3 @@
-{%- macro convert_tz (column_name, tz='{{var("timezone")}}', tz_alias='{{var("timezone_alias")}}') -%}
+{%- macro convert_tz (column_name, tz=var("timezone"), tz_alias=var("timezone_alias")) -%}
 	convert_timezone('{{tz}}', {{column_name}}) as {{column_name[(column_name.find('.')+1):] + '_' + tz_alias.lower()}}
 {%- endmacro -%}
